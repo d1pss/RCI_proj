1b41a61dd10d8d00fae197d5b6b4962c33c68e8d
d1pss <rcunhaoliveira.k@gmail.com>
2026-02-24 22:12:47 +0000

    a atualizar os cmd para darem return de error codes

--- main.c
+++ main.c
@@ -28,7 +28,7 @@ typedef struct _UDP_Server
 }UDP_S;
 
 
-void send_message_to_UDP_server(char* message, char** response, int* sizeof_response, UDP_S* Server);
+int send_message_to_UDP_server(char* message, char** response, int* response_len, UDP_S* Server);
 
 bool is_id_in_net(char* net, char* id, UDP_S* Server);
 
@@ -47,7 +47,13 @@ int main(void){
 }
 
 
-
+//return 0 return to main without error
+//return 1 return to main to exit the program without errors
+//return 2 return to main to exit the program with network error
+//return 3 return to main to exit the program with incorrect input
+//return 4 return to main to exit the program with memory allocation error
+//return 5 return to main to exit the program with connection via UDP or TCP error
+//return 6 return to main to exit the program with not suposed to happend case
 int process_command(char *input, Cmd_V* cmd_variables, UDP_S* Server){
     int num_args, return_code;
     char cmd[4];
@@ -101,12 +107,12 @@ int process_command(char *input, Cmd_V* cmd_variables, UDP_S* Server){
 
         if(!strcmp(cmd, "n")){
 
-            if(atoi(arguments[1]) > 999 || atoi(arguments[1]) < 0){
+            if(atoi(arguments[0]) > 999 || atoi(arguments[0]) < 0){
                 //error net value should be between 999 and 000
-                printf("ERROR:");
+                printf("ERROR: input argument net should be between 000 and 999\n");
             }
 
-            return_code = cmd_show_nodes(arguments[1], Server);
+            return_code = cmd_show_nodes(arguments[0], Server);
 
             if(return_code == 1){
                 //return to main to exit the program with network error
@@ -130,11 +136,11 @@ int process_command(char *input, Cmd_V* cmd_variables, UDP_S* Server){
 
         if(!strcmp(cmd, "j")){
 
-            if(atoi(arguments[1]) > 999 || atoi(arguments[1]) < 0 || atoi(arguments[2]) > 99 || atoi(arguments[2]) < 0){
+            if(atoi(arguments[0]) > 999 || atoi(arguments[0]) < 0 || atoi(arguments[1]) > 99 || atoi(arguments[1]) < 0){
                 //error net value should be between 999 and 000 end id between 99 and 00
             }
 
-            if(is_id_in_net(arguments[1], arguments[2], Server)){
+            if(is_id_in_net(arguments[0], arguments[1], Server)){
                 //error id already exists must use a difrent one
             }
 
@@ -170,49 +176,52 @@ int process_command(char *input, Cmd_V* cmd_variables, UDP_S* Server){
 
 int cmd_show_nodes(char* net, UDP_S* Server){
     char message[Max_message_len], *response, op;
-    int sizeof_response;
+    int response_len, return_code;
 
     sprintf(message, "NODES 100 0 %s\n", net);
 
-    send_message_to_UDP_server(message, &response, &sizeof_response, Server);
+    return_code = send_message_to_UDP_server(message, &response, &response_len, Server);
+
+    if(return_code != 0){
+        return return_code;
+    }
 
     int items_found = sscanf(response, "%*s %*s %c", &op);
 
     if (items_found == 1) {
         if (op == '1') {
             // message contains the ids in the network
-            print_ids(response, sizeof_response, net);
+            return_code = print_ids(response, response_len, net);
 
             free(response);
-            return 0;
+            return return_code;
         }else{
             //error code from network
             printf("ERROR: error code (%c) from network using this comand %s\n", op, message);
 
             free(response);
-            return 1;
+            return 2;
         }
-    }
-
-    
-    
-
+    }else{
+        printf("DEBUG ERROR: (in function cmd_show_nodes) if we are reading this the server sent a bad formated response (not suposed to do that)\n");
+        return 6;
+    } 
 }
 
-void print_ids(char* response, int sizeof_response, char* net){
+int print_ids(char* response, int response_len, char* net){
 
     int first_id_index = 0;
     char id_to_print[3] = {'0', '0', '\0'};
 
-    for(int i = 0; i < sizeof_response; i++){
+    for(int i = 0; i < response_len; i++){
         if(response[i] == '\n'){
             first_id_index = i + 1;
         }
     }
     if(first_id_index != 0){
-        if(first_id_index != sizeof_response){
+        if(first_id_index != response_len){
             printf("List of Nodes in network %s:\n", net);
-            for(int i = first_id_index; i < sizeof_response; i+3){
+            for(int i = first_id_index; i < response_len; i+3){
                 id_to_print[0] = response[i];
                 id_to_print[0] = response[i+1];
                 printf("%s\n", id_to_print);
@@ -224,61 +233,69 @@ void print_ids(char* response, int sizeof_response, char* net){
     }else{
         //response is not as expected
         printf("DEBUG ERROR: (in function print_ids) if we are reading this the server sent a bad formated response (not suposed to do that)\n");
+        return 6;
     }
     
-    return;
+    return 0;
 }
 
-void cmd_leave(){
+int cmd_leave(){
     //retira todas as arestas ligadas ao no e remove o no da rede
 }
 
-void cmd_join(char* net, char* id, UDP_S* Server){
+int cmd_join(char* net, char* id, UDP_S* Server){
     char message[Max_message_len], *response, op;
-    int sizeof_response;
+    int response_len, return_code;
 
     char* new_IP, *new_Port;
 
-    get_new_ip_and_Port(&new_IP, &new_Port);
+    //get_new_ip_and_Port(&new_IP, &new_Port);
     
     sprintf(message, "REG 100 0 %s %s %s %s\n", net, id, new_IP, new_Port);
 
-    send_message_to_UDP_server(message, &response, &sizeof_response, Server);
+    return_code = send_message_to_UDP_server(message, &response, &response_len, Server);
+
+    if(return_code != 0){
+        return return_code;
+    }
 
     int items_found = sscanf(response, "%*s %*s %c", &op);
 
     if (items_found == 1) {
         if (op == '1') {
             // id sucessfuly registred in network
+            printf("id was sucessfuly registred in network\n");
+
             free(response);
-            return;
+            return 0;
         } else if (op == '2') {
             // Network is full
             free(response);
-            return;
+            return 0;
         }else{
             //error code from network
+            printf("ERROR: error code (%c) from network using this comand %s\n", op, message);
+
+            free(response);
+            return 2;
         }
+    }else{
+        //response is not as expected
+        printf("DEBUG ERROR: (in function cmd_join) if we are reading this the server sent a bad formated response (not suposed to do that)\n");
+        return 6;
     }
-
-    free(response);
-    return;
-
 }
 
-void get_new_ip_and_Port(char** new_IP, char**new_Port){
-
-}
 
 bool is_id_in_net(char* net, char* id, UDP_S* Server){
     char message[Max_message_len];
     char* response, op;
-    int sizeof_response;
+    int response_len;
 
     sprintf(message, "CONTACT 100 0 %s %s\n", net, id);
    
 
-    send_message_to_UDP_server(message, &response, &sizeof_response, Server);
+    send_message_to_UDP_server(message, &response, &response_len, Server);
  
     int items_found = sscanf(response, "%*s %*s %c", &op);
 
@@ -301,8 +318,8 @@ bool is_id_in_net(char* net, char* id, UDP_S* Server){
 }
 
 
-//response needs to be freed outside the function 
-void send_message_to_UDP_server(char* message, char** response, int* sizeof_response, UDP_S* Server){
+//response needs to be freed outside the function (if it has error no need to free response)
+int send_message_to_UDP_server(char* message, char** response, int* response_len, UDP_S* Server){
     int fd,errcode;
     ssize_t n;
     socklen_t addrlen;
@@ -311,37 +328,39 @@ void send_message_to_UDP_server(char* message, char** response, int* sizeof_resp
     char buffer[Max_buff_size];
 
     fd=socket(AF_INET,SOCK_DGRAM,0); //UDP socket
-    if(fd==-1) /*error*/exit(1);
+    if(fd==-1) /*error*/return 5;
 
     memset(&hints,0,sizeof hints);
     hints.ai_family=AF_INET; //IPv4
     hints.ai_socktype=SOCK_DGRAM; //UDP socket
 
     errcode=getaddrinfo(Server->IP,Server->Port,&hints,&res);
-    if(errcode!=0) /*error*/ exit(1);
+    if(errcode!=0) /*error*/ return 5;
 
     n=sendto(fd,message,strlen(message),0,res->ai_addr,res->ai_addrlen);
-    if(n==-1) /*error*/ exit(1);
+    if(n==-1) /*error*/ return 5;
 
     addrlen=sizeof(addr);
     n=recvfrom(fd,buffer,Max_buff_size,0,(struct sockaddr*)&addr,&addrlen);
-    if(n==-1) /*error*/ exit(1);
+    if(n==-1) /*error*/ return 5;
 
     if(n < Max_buff_size){
         buffer[n] = '\0';
     }else{
-        //lost info not suposed to happen need bigger buffer
+        //lost info not suposed to happen if it happends we need bigger buffer
         printf("SE ISTO IMPRIMIO PERCISAMOS DE UM BUFFER MAIOR\n"); //debug retirar no fim
     }
 
     *response = (char*)malloc((n+1)*sizeof(char));
-    if(response == NULL){
+    if(*response == NULL){
         //error alocating memory
-        exit(1);
+        return 4;
     }
-    *sizeof_response = n;
+    *response_len = n;
     strcpy(*response, buffer);
 
     freeaddrinfo(res);
     close(fd);
+
+    return 0;
 }
